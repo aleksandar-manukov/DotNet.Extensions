fb680eb2136852f3558295171c55f3043f52462f
Aleksandar Manukov <34056917+aleksandar-manukov@users.noreply.github.com>
2019-06-01 15:06:24 +0300

    Add ToCamelCase method to StringExtensions

---
+++ Src/DotNet.Extensions.System.Extensions/StringExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+
+namespace DotNet.Extensions.System.Extensions
+{
+    /// <summary>
+    /// Class containing string extensions.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Converts any string to camel case.
+        /// </summary>
+        /// <param name="text">String to be converted to camel case string.</param>
+        /// <returns></returns>
+        /// <example>
+        /// This is an example, showing how to use <see cref="ToCamelCase(string)"/>.
+        /// <code>
+        /// string test = "This is a test string.";
+        ///
+        /// string camelCaseTest = text.ToCamelCase();
+        ///
+        /// Console.WriteLine(camelCaseTest); // thisIsATestString
+        /// </code>
+        /// </example>
+        public static string ToCamelCase(this string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                throw new ArgumentNullException(string.Empty, "Text cannot be null, empty or contains only white spaces.");
+            }
+
+            StringBuilder sb = new StringBuilder();
+            bool shouldBeLowerCase = true;
+            bool shouldBeUpperCase = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetterOrDigit(text[i]))
+                {
+                    if (shouldBeLowerCase)
+                    {
+                        sb.Append(text[i].ToString().ToLower());
+
+                        shouldBeLowerCase = false;
+                    }
+                    else if (shouldBeUpperCase)
+                    {
+                        sb.Append(text[i].ToString().ToUpper());
+
+                        shouldBeUpperCase = false;
+                    }
+                    else
+                    {
+                        sb.Append(text[i]);
+                    }
+                }
+                else if (!shouldBeLowerCase)
+                {
+                    shouldBeUpperCase = true;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tests/DotNet.Extensions.System.Extensions.Tests/StringExtensionsTests.cs
@@ -0,0 +1,50 @@
+using Xunit;
+
+namespace DotNet.Extensions.System.Extensions.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("thisIsAlreadyCamelCase", "thisIsAlreadyCamelCase")]
+        [InlineData("AfterTheCallWillBeCamelCase", "afterTheCallWillBeCamelCase")]
+        [InlineData("thisWordContainsDigits123", "thisWordContainsDigits123")]
+        public void ToCamelCase_Method_Should_Convert_One_Word_String_To_Camel_Case(string text, string expectedResult)
+        {
+            // Act
+            string actualResult = text.ToCamelCase();
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Theory]
+        [InlineData("This is a sentence.", "thisIsASentence")]
+        [InlineData("This is a longer sentence, containing comma in it.", "thisIsALongerSentenceContainingCommaInIt")]
+        [InlineData(" - \"This is a direct speech.\" - author", "thisIsADirectSpeechAuthor")]
+        [InlineData("Digits in the sentence are kept - 1, 2, 3 etc.", "digitsInTheSentenceAreKept123Etc")]
+        public void ToCamelCase_Method_Should_Convert_One_Sentence_String_To_Camel_Case(string text, string expectedResult)
+        {
+            // Act
+            string actualResult = text.ToCamelCase();
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Theory]
+        [InlineData(@"This is the first paragraph.
+
+                This is the second paragraph.", "thisIsTheFirstParagraphThisIsTheSecondParagraph")]
+        [InlineData(@" - This is the first paragraph.
+
+                 - This is the second paragraph, containing digits - 1, 2, 3 etc.", "thisIsTheFirstParagraphThisIsTheSecondParagraphContainingDigits123Etc")]
+        public void ToCamelCase_Method_Should_Convert_One_Paragraph_String_To_Camel_Case(string text, string expectedResult)
+        {
+            // Act
+            string actualResult = text.ToCamelCase();
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
