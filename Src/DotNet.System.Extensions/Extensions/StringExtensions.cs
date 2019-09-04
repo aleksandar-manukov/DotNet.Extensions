813e9cd1b78a46bd5306491ce3de732e8da1d94c
Aleksandar Manukov <34056917+aleksandar-manukov@users.noreply.github.com>
2019-09-04 20:32:55 +0300

    Add ToPascalCase method to StringExtensions

--- Src/DotNet.System.Extensions/Extensions/StringExtensions.cs
+++ Src/DotNet.System.Extensions/Extensions/StringExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text;
 
 namespace DotNet.System.Extensions
@@ -17,11 +17,11 @@ namespace DotNet.System.Extensions
         /// <example>
         /// This is an example, showing how to use <see cref="ToCamelCase(string)"/>.
         /// <code>
-        /// string test = "This is a test string.";
+        /// string testString = "This is a test string.";
         ///
-        /// string camelCaseTest = text.ToCamelCase();
+        /// string camelCaseTestString = testString.ToCamelCase();
         ///
-        /// Console.WriteLine(camelCaseTest); // thisIsATestString
+        /// Console.WriteLine(camelCaseTestString); // thisIsATestString
         /// </code>
         /// </example>
         public static string ToCamelCase(this string text)
@@ -56,6 +56,54 @@ namespace DotNet.System.Extensions
                     }
                 }
                 else if (!shouldBeLowerCase)
+                {
+                    shouldBeUpperCase = true;
+                }
+            }
+
+            return sb.ToString();
+        }
+        /// <summary>
+        /// Converts any string to pascal case.
+        /// </summary>
+        /// <param name="text">String to be converted to pascal case string.</param>
+        /// <returns>String in pascal case.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when string is null, empty or contains only white spaces.</exception>
+        /// <example>
+        /// This is an example, showing how to use <see cref="ToPascalCase(string)"/>.
+        /// <code>
+        /// string testString = "This is a test string.";
+        ///
+        /// string pascalCaseTestString = testString.ToPascalCase();
+        ///
+        /// Console.WriteLine(pascalCaseTestString); // ThisIsATestString
+        /// </code>
+        /// </example>
+        public static string ToPascalCase(this string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                throw new ArgumentNullException(nameof(text), "Text cannot be null, empty or contains only white spaces.");
+            }
+
+            StringBuilder sb = new StringBuilder();
+            bool shouldBeUpperCase = true;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetterOrDigit(text[i]))
+                {
+                    if (shouldBeUpperCase)
+                    {
+                        sb.Append(text[i].ToString().ToUpper());
+
+                        shouldBeUpperCase = false;
+                    }
+                    else
+                    {
+                        sb.Append(text[i]);
+                    }
+                }
+                else
                 {
                     shouldBeUpperCase = true;
                 }
--- Tests/DotNet.System.Extensions.Tests/StringExtensionsTests.cs
+++ Tests/DotNet.System.Extensions.Tests/StringExtensionsTests.cs
@@ -46,5 +46,48 @@ namespace DotNet.System.Extensions.Tests
             // Assert
             Assert.Equal(expectedResult, actualResult);
         }
+
+        [Theory]
+        [InlineData("ThisIsAlreadyPascalCase", "ThisIsAlreadyPascalCase")]
+        [InlineData("afterTheCallWillBePascalCase", "AfterTheCallWillBePascalCase")]
+        [InlineData("thisWordContainsDigits123", "ThisWordContainsDigits123")]
+        public void ToPascalCase_Method_Should_Convert_One_Word_String_To_Pascal_Case(string text, string expectedResult)
+        {
+            // Act
+            string actualResult = text.ToPascalCase();
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Theory]
+        [InlineData("This is a sentence.", "ThisIsASentence")]
+        [InlineData("This is a longer sentence, containing comma in it.", "ThisIsALongerSentenceContainingCommaInIt")]
+        [InlineData(" - \"This is a direct speech.\" - author", "ThisIsADirectSpeechAuthor")]
+        [InlineData("Digits in the sentence are kept - 1, 2, 3 etc.", "DigitsInTheSentenceAreKept123Etc")]
+        public void ToPascalCase_Method_Should_Convert_One_Sentence_String_To_Pascal_Case(string text, string expectedResult)
+        {
+            // Act
+            string actualResult = text.ToPascalCase();
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Theory]
+        [InlineData(@"This is the first paragraph.
+
+                This is the second paragraph.", "ThisIsTheFirstParagraphThisIsTheSecondParagraph")]
+        [InlineData(@" - This is the first paragraph.
+
+                 - This is the second paragraph, containing digits - 1, 2, 3 etc.", "ThisIsTheFirstParagraphThisIsTheSecondParagraphContainingDigits123Etc")]
+        public void ToPascalCase_Method_Should_Convert_One_Paragraph_String_To_Pascal_Case(string text, string expectedResult)
+        {
+            // Act
+            string actualResult = text.ToPascalCase();
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
     }
 }
